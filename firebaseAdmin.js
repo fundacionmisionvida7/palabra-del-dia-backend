c55e25d3314cc464ae12743625353070cb2cd414
fundacionmisionvida7 <fundacionmisionvida7@gmail.com>
2025-04-19 19:10:47 -0300

    Update firebaseAdmin.js

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,23 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+let serviceAccount;
+
+try {
+  const raw = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_B64, 'base64').toString('utf8');
+  serviceAccount = JSON.parse(raw);
+} catch (e) {
+  console.error('❌ Error cargando clave Firebase:', e);
+  throw e;
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
+  console.log("✅ Firebase Admin inicializado correctamente");
+} else {
+  console.log("⚠️ Firebase Admin ya estaba inicializado");
 }
 
 export default admin;
+
